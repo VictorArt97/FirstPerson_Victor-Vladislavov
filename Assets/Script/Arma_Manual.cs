f89d7cdab1d31edcbd4e66083e27fbf4eb705edd
VictorArt97 <Bakalovvictor@gmail.com>
2024-11-06 21:27:00 +0100

    particulas 3

--- Assets/Script/Arma_Manual.cs
+++ Assets/Script/Arma_Manual.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Arma_Manual : MonoBehaviour
 {
+    [SerializeField] ParticleSystem system;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,8 @@ public class Arma_Manual : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("pium");
+           system.Play();
+
         }
 
     }
